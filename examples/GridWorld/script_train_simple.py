c72ec311a88b23fbfc8470e7dba26f2080378823
li-ming-fan <li_m_f@163.com>
2020-04-05 12:54:48 +0800

    修改：     README.md 	修改：     examples/GridWorld/script_train_simple.py 	修改：     zhiqiang/README.md

--- examples/GridWorld/script_train_simple.py
+++ examples/GridWorld/script_train_simple.py
@@ -2,24 +2,24 @@
 
 # define an env
 from grid_world import GridWorld as Env
-#
+
 # define a qnet, in PyTorch
 from gridworld_qnet import GridWorldQNet as QNet
-#
+
 # pick an agent
 from zhiqiang.agents.dqn_vanila import VanilaDQN as Agent
 # from zhiqiang.agents.dqn_double import DoubleDQN as Agent
-#
+
 # pick a buffer
 from zhiqiang.replay_buffers.simple_buffer import SimpleBuffer as Buffer
-#
+
 # pick a trainer
 from zhiqiang.trainers.simple_trainer import SimpleTrainer as Trainer
-#
+
 # settings file
 settings_filepath = "./examples/GridWorld/settings_dqn.json"
-#
 
+##
 # The following is a common routine.
 from zhiqiang.utils.basic_settings import BasicSettings
 #
